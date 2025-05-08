7eb559979921e7cfdd5116ca9cb8dd85748e8092
daniellima34 <danielgomes29031991@gmail.comm>
2025-05-08 11:54:01 -0300

    atualização do filme

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,13 +4,17 @@ import { BsFilm } from "react-icons/bs";
 export default function Header({showButton = true}){
     return(
         <header className="w-full h-[50px] flex bg-[#0C0F15] justify-between pr-2 items-center">
-            <div className="flex h-full gap-2 items-center justify-cente pl-2">
+            <div 
+            onClick={() => window.location.href = "/"}
+            className="flex h-full gap-2 items-center justify-cente pl-2 cursor-pointer">
             <BsFilm color="#9B87f5" size={25}/>
             <h2 className="font-bold text-[20px] text-white">Darck Filme</h2>
 
             </div>
             {showButton &&(
-                <button className="w-[150px] h-[60%] rounder-md bg-[#9b87f5] text-white font-bold hover:bg-[#9b87f5]/70 ">Adicionar Filme</button>
+                <button 
+                onClick={()=> window.location.href="/cadastrar"}
+                className="w-[150px] h-[60%] rounder-md bg-[#9b87f5] text-white font-bold hover:bg-[#9b87f5]/70 ">Adicionar Filme</button>
             )}
 
         </header>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,22 @@
+import CardFilmes from "@/components/CardFilme";
 import PageWrapper from "@/components/PageWrapper";
 
 
 export default function Home(){
   return (
-    PageWrapper
+   <PageWrapper>
+    <div className="w-full flex flex-col items-center justify-center pt-7">
+      <h1 className=" text-[40px]  text-[#907cda] font-bold ">sua coleção de filmes</h1>
+      <p className=" text-[#9a898c] text-[20px]">acompanhe seus filmes favoritos, Adicione novos filmes a sua coleção</p>
+    </div>
+    <div className="w-full h-auto flex flex-wrap justify-center">
+      <CardFilmes/>
+    </div>
+    
+    
+   </PageWrapper>
+
+  
   
   )
 }
